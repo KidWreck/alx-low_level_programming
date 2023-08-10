9c4f52de63a385cb8e2bc706dd141acada713445
Hassan <blaugrana888@hotmail.com>
2023-08-10 02:45:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+/**
+ * main - Entry void
+ * Description: using puts
+ * Return: Always postive (0)
+*/
+int main(void)
+{
+	char Dora[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.Dora.59);
+	return (1);
+}
