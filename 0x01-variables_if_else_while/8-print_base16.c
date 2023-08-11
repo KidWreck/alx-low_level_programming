431728bd224d4aebe8c6e441a6af2647cb0051a9
Hassan <blaugrana888@hotmail.com>
2023-08-11 09:56:20 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  * main - Entry void
+ *   * Description: using puts
+ *    * Return: Always postive (0)
+*/
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 16)
+	{
+		putcharr(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
