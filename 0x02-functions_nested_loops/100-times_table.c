3f293806c2428ca89be1cf30a73c3a9bc7d9ccd1
Hassan <blaugrana888@hotmail.com>
2023-08-15 05:54:31 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 	for (i = 0; i <= n ; i++)
-	{
+		{
 		for (j = 0; j <= n; j++)
 		{
 			k = i * j;
@@ -44,6 +44,7 @@ void print_times_table(int n)
 				_putchar((k % 10) + 48);
 			}
 		_putchar('\n');
-	}
-	}
+		}
+		}
+}
 }
