091b3e94155dcc76ba292dac4f958ec2ec85d92b
Hassan <blaugrana888@hotmail.com>
2023-08-17 08:06:52 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multi two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multi result
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
