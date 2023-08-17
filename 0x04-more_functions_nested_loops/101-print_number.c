546e17bedc08d062fe36a04b85ec508afee13f29
Hassan <blaugrana888@hotmail.com>
2023-08-17 16:46:43 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-	int k = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
@@ -19,6 +19,5 @@ void print_number(int n)
 	k /= 10;
 	if (k != 0)
 		print_number(k);
-	_putchar((n % 10) + '0');
-	_putchar('\n');
+	_putchar((unsigned int) n % 10 + '0');
 }
