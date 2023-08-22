3bd3c2f1dd5d254961d4f8b98b6fb1c20cbd93a6
Hassan <blaugrana888@hotmail.com>
2023-08-21 18:21:30 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 
 int _atoi(char *s)
 {
-	unsigned int n;
+	unsigned int n = 0;
 	int sign = 1;
 
 	do {
@@ -16,7 +16,7 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
-		else
+		else if (n > 0)
 			break;
 	} while (*s++);
 
