10de0ef6e76355a9ce5c07a6209306e3aa610888
Hassan <blaugrana888@hotmail.com>
2023-08-24 05:55:36 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - rotate the alphabet 13 char
+ * @a: string
+ * Return: a
+ */
+
+char *rot13(char *a)
+{
+	int i, j;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0 ; a[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 52 ; j++)
+		{
+			if (a[i] == data[j])
+			{
+				a[i] == rot[j];
+				break;
+			}
+		}
+	}
+	return (a);
+}
