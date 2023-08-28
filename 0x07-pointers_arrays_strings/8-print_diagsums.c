ba8756c9db6c614ee469ec6243688d2be6c020d9
Hassan <blaugrana888@hotmail.com>
2023-08-28 16:43:54 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum the diag and the diag reverse
+ * @a: matrix 2D
+ * @size: x=y
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, add = 0, dda = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		add += a[i];
+		dda += a[size - i - 1];
+		a += size;
+	}
+	printf("%d , %d\n", add, dda);
+}
