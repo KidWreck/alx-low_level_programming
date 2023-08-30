0a82cf76897239d62242898e98195ca78b967172
Hassan <blaugrana888@hotmail.com>
2023-08-30 15:02:14 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - count char in string
+ * @s: pointer to string
+ * Return: number of chars
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s > '\0')
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+	return (i);
+}
