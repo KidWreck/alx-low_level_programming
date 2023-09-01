598adf9566bdc8030557826ea24f90f03550de0d
Hassan <blaugrana888@hotmail.com>
2023-09-01 15:36:22 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: always 0
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 	(void)argc;
 
 	return (0);
