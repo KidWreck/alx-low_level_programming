cf4a4752dc64a6edc9e185169b50bb8cedcc1eea
Hassan <blaugrana888@hotmail.com>
2023-09-01 15:55:32 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multi two int
+ * @argc: number of args
+ * @argv: array of args
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	return (0);
+}
