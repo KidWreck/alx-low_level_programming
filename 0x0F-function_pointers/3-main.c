d75cd10000a7c56bf4f022d49fdde30cf7abe930
Hassan <blaugrana888@hotmail.com>
2023-09-13 18:36:03 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - simple calc
+ * @argc: number of arg
+ * @argv: array of arg
+ * Return: 0 if true, else if Error
+ */
+int main(int argc, char **argv[])
+{
+	int (*op)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op = get_op_func(argv[2]);
+	if (!op)
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+
+	printf("%d\n", op(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two int
+ * @a: first int
+ * @b: second int
+ * Return: add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two int
+ * @a: first int
+ * @b: second int
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul two int
+ * @a: first int
+ * @b: second int
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div two int
+ * @a: first int
+ * @b: second int
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module of two int
+ * @a: first int
+ * @b: second int
+ * Return: module
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
