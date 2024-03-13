e71c758884fe311ebfd1947a35d015ca1d1d6ec8
KidWreck <139178012+KidWreck@users.noreply.github.com>
2024-03-13 05:43:50 +0200

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,3 +1,49 @@
+#include "search_algos.h"
+/**
+ * b_search - Binary search algorithm
+ *
+ * @array: Array.
+ * @low: Left index in the array.
+ * @high: right index in the array.
+ * @value: Value to search for in the array.
+ *
+ * Return: The first index or -1 if NULL.
+ */
+int b_search(int *array, size_t low, size_t high, int value)
+{
+	size_t left = low;
+	size_t right = high - 1;
+	size_t middle = 0;
+	size_t i = 0;
+
+	if (array != NULL)
+	{
+		while (left <= right)
+		{
+			middle = (left + right) / 2;
+			printf("Searching in array: ");
+			for (i = left; i < right; i++)
+			{
+				printf("%d, ", array[i]);
+			}
+			printf("%d\n", array[i]);
+			if (array[middle] < value)
+			{
+				left = middle + 1;
+			}
+			else if (array[middle] > value)
+			{
+				right = middle - 1;
+			}
+			else
+			{
+				return (middle);
+			}
+		}
+	}
+	return (-1);
+}
+
 /**
  * exponential_search - Exponential search algorithm
  *
